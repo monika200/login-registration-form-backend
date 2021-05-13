2c0a3d44af3df54170ec4c99384d121b2498f861
Monika Sharma <monika2004sharma@gmail.com>
2021-05-13 21:21:36 +0530

    update

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ const dbURL = process.env.dbURL
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(cors({
-    origin:"https://eazr-login-registration-form.netlify.app/"
-}))
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log('your app is running wonderfully at', port))
